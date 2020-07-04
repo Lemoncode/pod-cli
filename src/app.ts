1382d90194be8bdb7f5aed939c19eb8bca4dc2ac
nasdan <daniel.sanchez@lemoncode.net>
2020-07-04 21:22:47 +0200

    commit from linux

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import { prompt } from 'inquirer';
 import { questionList } from './questions';
+import os from 'os'
+
 (async () => {
   const { featureList } = await prompt(questionList);
-
+  console.log({ platform: os.platform() })
   console.log({ featureList });
 })();
